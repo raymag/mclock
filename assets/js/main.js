7fe9cf15a2c6ebe28686865caea0a945715cc958
raymag <prayk11@outlook.com>
2019-10-11 21:54:42 -0300

    mclock

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+function setClock(){
+    console.log('Setting clock...')
+    const hourPointer = document.querySelector('#hour-pointer')
+    const minutePointer = document.querySelector('#minute-pointer')
+    const timer = document.querySelector('#timer h1')
+
+    let time = Date.now()
+    let hours = new Date(time).getHours()
+    let minutes = new Date(time).getMinutes()
+
+    let hoursDegrees = 15*hours
+    let minutesDegrees = .25*-minutes
+
+    hourPointer.style.transform = `rotate(${hoursDegrees}deg) translate( 0, -35px )`
+    minutePointer.style.transform = `rotate(${minutesDegrees}deg) translate( 0, -60px )`
+
+    timer.innerText = `${hours}:${minutes}`
+}
+
+setInterval(setClock, 500)
